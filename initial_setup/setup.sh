888f49ecccae2e8035a55051377cf6e34f15ad2f
sablet <e.typ719@gmail.com>
2017-01-05 10:25:34 +0900

    setup.cfg modift

--- initial_setup/setup.sh
+++ initial_setup/setup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-ori_name=python_template
+ori_name=$(basename $(pwd))
 cd ..
 mv $ori_name $1
 cd $1
 virtualenv -p /usr/bin/python3 virenv_for_$1
-cat setup.py|sed "s/$ori_name/$1/g" > setup.py
+cp setup.py setup_temp.py
+sed "s/$ori_name/$1/g" setup_temp.py > setup.py
+rm setup_temp.py
 
 # data get
-cd tests/data
-data_url="http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip"
-fname=$(basename $data_url)
-wget $data_url
-unzip $fname
-rm $fname
+
+# cd tests/data
+# data_url="http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip"
+# fname=$(basename $data_url)
+# wget $data_url
+# unzip $fname
+# rm $fname
